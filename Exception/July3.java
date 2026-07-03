648b1dce843262439fd70659e01ef733cc469d14
Ebenezer <ebenezer6374@gmail.com>
2026-07-03 19:09:07 +0530

    Exception first step

---
+++ Exception/July3.java
@@ -0,0 +1,29 @@
+//package learnString;
+
+public class July3{
+    int no;
+    String st;
+    int price;
+
+    public July3(int no, String st, int price) {
+        this.no = no;
+        this.st = st;
+        this.price = price;
+    }
+//    public July3() {
+//
+//    }
+    public static void main(String[] args) {
+        July3 h1 = new July3(25,"kambar st",30);
+        July3 h2 = new July3(30,"seran st",50);
+        h2.comparePrice(h2);
+    }
+
+    public void comparePrice(July3 h) {
+        System.out.println(this.price);
+        System.out.println(h.price);
+
+    }
+
+
+}
---
+++ Practice/July2.java
@@ -0,0 +1,32 @@
+public class July2 {
+    int price =24;
+    int door = 86;
+    String street = "Velachery";
+    public July2 (int i , int j , String q){
+        this.price = price;
+        this.door = door;
+        this.street = street;
+    }
+
+    public July2 (){
+
+    }
+
+
+
+
+
+            public static void  main (String[] args){
+                July2 ch = new July2( 3 ,4, ,'hfuihue')
+                July2.Display();
+
+
+            }
+            public static void July2(July2 ch){
+        System.out.println
+            }
+
+
+
+}
+
