024036c7371c1263a998168c5f020a688078a2a3
Ebenezer <ebenezer6374@gmail.com>
2026-07-11 02:55:04 +0530

    file handling assignment

--- FileHandling/July10.java
+++ FileHandling/July10.java
@@ -5,22 +5,24 @@ public class July10{
     public static void main (String[] args) throws IOException {
         File file = new File("/home/buddingdeveloper/Desktop/Gpt product/ram.text");
         file.createNewFile();
+        file.delete();
 
+//        FileWriter pen = new FileWriter(file);
+//        BufferedWriter writer = new BufferedWriter(pen);
+//        writer.write("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n");
+//        writer.write("Ganesh\n");
+//        writer.close();
 
-        FileWriter pen = new FileWriter(file);
-        BufferedWriter writer = new BufferedWriter(pen);
-        writer.write("karthik");
-        writer.write("Ganesh");
-        writer.close();
-
-        FileReader read =new FileReader(file);
-        BufferedReader ch = new BufferedReader(read);
-
-        String str = ch.readLine();
-        while (str!= null){
-            System.out.println(str);
-            str = ch.readLine();
-        }
+//        FileReader read = new FileReader(file);
+////        BufferedReader ch = new BufferedReader(read);
+//
+//        int ch = read.read();
+//        int count = 0;
+//        while (count <= 100) {
+//            System.out.println((char) ch);
+//            ch = read.read();
+//            count++;
+//        }
 
     }
 }
